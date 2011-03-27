03002fe41fe728c1ecf8b7b568e445d9f216b658
Chanpory Rith <hello@chanpory.com>
2011-03-26 23:33:25 -0700

    Initial setup of middleman

--- config.rb
+++ config.rb
@@ -1,11 +1,58 @@
-# Require any additional compass plugins here.
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "src"
-images_dir = "images"
-javascripts_dir = "js"
-line_comments = false
-output_style = :compact
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+# Automatic sitemaps
+# activate :slickmap
+
+# CodeRay syntax highlighting in Haml
+# activate :code_ray
+
+# Automatic image dimension calculations
+# activate :automatic_image_sizes
+
+# Per-page layout changes
+# With no layout
+# page "/path/to/file.html", :layout => false
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+
+# Helpers
+helpers do
+  def some_helper(*args)
+    "Helping"
+  end
+end
+
+
+# Change the CSS directory
+# set :css_dir, "alternative_css_directory"
+
+
+
+# Change the JS directory
+# set :js_dir, "alternative_js_directory"
+
+
+
+# Change the images directory
+# set :images_dir, "alternative_image_directory"
+
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+  
+  # Minify Javascript on build
+  # activate :minify_javascript
+  
+  # Shrink/smush PNG/JPEGs on build
+  # activate :smush_pngs
+  
+  # Enable cache buster
+  # activate :cache_buster
+
+  # Generate ugly/obfuscated HTML from Haml
+  # activate :ugly_haml
+
+  
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
